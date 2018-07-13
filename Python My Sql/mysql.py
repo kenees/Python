b67f8b39a92a2deb5fee28a7a1f0e8c209233282
Jreay <wangcheng@outlook.com>
2018-07-13 16:29:35 +0800

    魔法方法

---
+++ Python My Sql/mysql.py
@@ -0,0 +1,30 @@
+#配置好环境变量
+#打开cmd  输入指令 pip install package
+#安装pymysql pip install pymysql
+import pymysql
+
+#打开数据库连接
+db = pymysql.connect(host='10.19.105.146',user='root',password='o83ennArmZd2dXn7',db='node',port=3306)
+
+#使用cursor()方法获取操作游标
+cur = db.cursor()
+
+#1.查询操作
+# 编写sql 查询语句  user 对应我的表名
+sql = 'select * from user'
+try:
+    cur.execute(sql)            #执行sql语句
+    results = cur.fetchall()    #获取查询的所有记录
+    print('id','name','password')
+    #遍历结果
+    for row in results:
+        id = row[0]
+        name = row[1]
+        password = row[2]
+        print(id,name,password)
+except Exception as e:
+    raise e
+finally:
+    db.close()        #关闭连接
+
+
